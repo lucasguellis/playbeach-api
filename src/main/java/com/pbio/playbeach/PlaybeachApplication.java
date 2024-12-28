70ecb6912b451d4f7d9859a6765447e35faf8b21
Lucas Guellis de Souza <lucasguellis.souza@gmail.com>
2024-12-28 11:12:08 -0300

    Starting spring api

---
+++ src/main/java/com/pbio/playbeach/PlaybeachApplication.java
@@ -0,0 +1,13 @@
+package com.pbio.playbeach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaybeachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaybeachApplication.class, args);
+	}
+
+}
