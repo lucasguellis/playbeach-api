4090dcaafb16ea30679d935e8edabd412718a27c
Lucas Guellis de Souza <lucasguellis.souza@gmail.com>
2024-12-28 11:11:30 -0300

    Adding tests

---
+++ src/test/java/com/pbio/playbeach/PlaybeachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbio.playbeach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaybeachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
